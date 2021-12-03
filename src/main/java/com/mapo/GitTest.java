1435a3307b79c89128fa478c334e9e47309a9eaf
mark <mark@163.com>
2021-12-03 16:11:04 +0800

    分支第n次提交修复

--- src/main/java/com/mapo/GitTest.java
+++ src/main/java/com/mapo/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("分支提交1次");
         System.out.println("123456");
         System.out.println("123456789");
+        System.out.println("githubb");
     }
 }
